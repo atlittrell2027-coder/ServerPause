0bb3a93f6535369cdfe0fd32ed1aba560a8c4d5f
Chemmaster73 <atlittrell2027@gmail.com>
2026-06-12 06:54:04 +0000

    Upddated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,21 @@
 plugins {
     java
 }
+
 group = "org.example"
 version = "1.0"
+
 repositories {
     mavenCentral()
+    // This is the active official repository url for PaperMC
     maven("https://papermc.io") 
 }
+
 dependencies {
+    // Correct target mapping for the modern 26.1.x snapshot architecture
     compileOnly("io.papermc.paper:paper-api:26.1.2-R0.1-SNAPSHOT")
 }
+
 tasks.jar {
     archiveFileName.set("ServerPause.jar")
 }
